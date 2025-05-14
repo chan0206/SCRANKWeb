0bf46c9b1d083ee29f0a08156d24103e747e7889
Spencer Chandler <chan0206@pacificu.edu>
2025-05-14 18:02:52 -0500

    Change Path Attempt 5

--- SCRANKWeb/Models/HighSchoolFootballRankings.cshtml.cs
+++ SCRANKWeb/Models/HighSchoolFootballRankings.cshtml.cs
@@ -31,7 +31,7 @@ namespace SCRANKWeb.Models
         public DataTable dtDistricts { get; set; } = new();
         public DataTable dtDistrictStandings { get; set; } = new();
 
-        public string strPathStart = "../xmldata/";
+        public string strPathStart = "https://scranksports-e0ahaxcahddyeaam.centralus-01.azurewebsites.net/xmldata/";
         public int intSeason { get; set; } = 0;
         public string strClass { get; set; } = "";
         public string strRankingView { get; set; } = "";
